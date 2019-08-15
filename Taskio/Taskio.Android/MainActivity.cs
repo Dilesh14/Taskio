90f54888702c1229c89a83331588dcb33a3e9cfc
Dilesh <drj855@mun.ca>
2019-08-15 21:20:02 -0230

    Splashscreen

--- Taskio/Taskio.Android/MainActivity.cs
+++ Taskio/Taskio.Android/MainActivity.cs
@@ -20,7 +20,7 @@ using System.IO;
 
 namespace Taskio.Droid
 {
-    [Activity(Label = "Taskio", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Taskio", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity, ActivityCompat.IOnRequestPermissionsResultCallback
     {
         internal static MainActivity Instance { get; private set; }
--- Taskio/Taskio/Views/SwipableView.xaml.cs
+++ Taskio/Taskio/Views/SwipableView.xaml.cs
@@ -14,10 +14,14 @@ namespace Taskio.Views
     public partial class SwipableView : ContentPage
     {
         private ViewModelForSwipe _viewModel { get; set; }
+        private ImageLoadingViewModel _SingleImageVM { get; set; }
         public SwipableView(ViewModelForSwipe vm)
         {
             InitializeComponent();
             _viewModel = vm;
+            _SingleImageVM = new ImageLoadingViewModel(_viewModel.SelectedItem.Name);
+            //Image image = this.FindByName<Image>("Picture");
+            //image.Source = _SingleImageVM.Source;
             SwipeContainer.SetBinding(PanCardView.CardsView.SelectedItemProperty, nameof(ViewModelForSwipe.SelectedItem));
             SwipeContainer.BindingContext = _viewModel;
         }
